09d549f905cbec873312195d81320cf5d26b27c9
HexHounds <38847793+hexhounds@users.noreply.github.com>
2019-02-15 22:04:56 -0600

    Added WORKING ColorSensor readings!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,22 +2,25 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import frc.robot.subsystems.ColorSensor;
 import frc.robot.subsystems.KiwiDriveTrain;
 
 public class Robot extends TimedRobot {
   public static OI oi = new OI();
 
   public static KiwiDriveTrain driveTrain;
+  public static ColorSensor colorSensor;
   // = new KiwiDriveTrain();
 
   @Override
   public void robotInit() {
+    colorSensor = new ColorSensor();
   }
 
   @Override
   public void robotPeriodic() {
     Scheduler.getInstance().run();
-    System.out.println(oi.gyro.getAngle());
+    // System.out.println(oi.gyro.getAngle());
   }
 
   @Override
@@ -47,6 +50,10 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
+    System.out.println("Blue " + Robot.colorSensor.blue());
+    System.out.println("Red " + Robot.colorSensor.red());
+    System.out.println("Green " + Robot.colorSensor.green());
+    System.out.println("Proximity " + Robot.colorSensor.proximity());
   }
 
   @Override
